d2e254ebfacf7dea540d46f3d9da967f98547a5d
QuintenDV <quintendevos3@hotmail.com>
2021-03-06 22:05:36 +0100

    Reworked the messages. Using enums now.

--- src/message.rs
+++ src/message.rs
@@ -1,185 +1,113 @@
 use serde_json::Value;
 use serde::{Deserialize, Serialize};
 
+
+pub type RawMessage = String;
+
+#[serde(tag = "type")]
+#[derive(Serialize, Deserialize,Debug,PartialEq)]
+pub enum XMessage {
+	Action			{ game: i32, key: String, action: Action},
+	Error			{ },
+	Register		{ clientType: String, game: String, name: String},
+	Start			{ game: i32, players: Vec<i32>, prefix: String, suffix: String},
+	State 			{ game: i32, key: String, turn: i32, r#move: bool, state: State},
+	Stop 			{ game: i32},
+	EngineAction	{game: i32, player: String, action: Action},
+	EngineState		{game: i32, turn: i32, players: Vec<String>, state: State},
+}
+
 type Action = Value;
 type State = Value;
-
-#[derive(Deserialize, Serialize)]
-struct ActionMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-	key: String,
-	action: Action,
-}
-
-#[derive(Deserialize, Serialize)]
-struct ActionMessageEngine {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-	player: String,
-	action: Action,
-}
-
-#[derive(Deserialize, Serialize)]
-struct ErrorMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct RegisterMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-	#[serde(rename = "clientType")]
-	client_type: String,
-	game: String,
-	name: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct StartMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-	players: Vec<i32>,
-	prefix: String,
-	suffix: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct StateMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-	key: String,
-	turn: i32,
-	r#move: bool,
-	state: State,
-}
-
-#[derive(Deserialize, Serialize)]
-struct StateMessageEngine {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-	turn: i32,
-	players: Vec<String>,
-	state: State,
-}
-
-#[derive(Deserialize, Serialize)]
-struct StopMessage {
-	#[serde(rename = "type")]
-	msgType: String,
-	game: i32,
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+	use serde_json::json;
 
-	fn reserialize(message: &impl serde::Serialize, json: &str) -> bool {
-		if let Ok(serialized) = serde_json::to_string(message){
-				if serialized == json {
-					return true;
-				}
-				println!("Result:   {}", serialized);
-				println!("Expected: {}", json);
-		}
-		return false;
+	fn reserialize(msg: XMessage) {
+		let json = serde_json::to_string(&msg).unwrap();
+		let target: XMessage = serde_json::from_str(&json).unwrap();
+
+		assert_eq!(target, msg);
 	}
 
     #[test]
     fn test_action_message() {
-		let json = r#"{"type":"MSG","game":42,"key":"KEY","action":{"TEST":"TEST"}}"#;
-        let message: ActionMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.key,"KEY");
-		assert_eq!(message.game,42);
-    }
-
-	#[test]
-    fn test_action_message_engine() {
-		let json = r#"{"type":"MSG","game":42,"player":"PLAYER","action":{"TEST":"TEST"}}"#;
-        let message: ActionMessageEngine = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.player,"PLAYER");
-		assert_eq!(message.game,42);
-    }
+		let message = XMessage::Action{
+			game: 4882,
+			key: "key".to_string(),
+			action: json!({"This can be": "anything"}),
+		};
+		reserialize(message);
+	}
 
 	#[test]
     fn test_error_message() {
-		let json = r#"{"type":"ERROR"}"#;
-        let message: ErrorMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"ERROR");
+		let message = XMessage::Error{};
+		reserialize(message);
     }
 
 	#[test]
-    fn test_register_message() {
-		let json = r#"{"type":"MSG","clientType":"CLIENTTYPE","game":"GAMENAME","name":"NAME"}"#;
-        let message: RegisterMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.game,"GAMENAME");
-		assert_eq!(message.client_type,"CLIENTTYPE");
-		assert_eq!(message.name,"NAME");
-    }
+	fn test_action_message_engine() {
+	let message = XMessage::EngineAction{
+			game: 4882,
+			player: "key".to_string(),
+			action: json!({"This can be": "anything"}),
+		};
+		reserialize(message);
+	}
 
 	#[test]
-    fn test_start_message() {
-		let json = r#"{"type":"MSG","game":42,"players":[0,1],"prefix":"PREFIX","suffix":"SUFFIX"}"#;
-        let message: StartMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.game,42);
-		assert_eq!(message.players,[0,1]);
-		assert_eq!(message.prefix,"PREFIX");
-		assert_eq!(message.suffix,"SUFFIX");
-    }
+	fn test_register_message() {
+	let message = XMessage::Register{
+			clientType: "clientType".to_string(),
+			game: "game".to_string(),
+			name: "name".to_string(),
+		};
+		reserialize(message);
+	}
 
 	#[test]
-    fn test_state_message() {
-		let json = r#"{"type":"MSG","game":42,"key":"KEY","turn":0,"move":true,"state":{"STATE":{}}}"#;
-        let message: StateMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.game,42);
-		assert_eq!(message.key,"KEY");
-		assert_eq!(message.turn,0);
-		assert_eq!(message.r#move,true);
-    }
+	fn test_start_message() {
+	let message = XMessage::Start{
+			game: 42,
+			players: vec![0,1],
+			prefix: "prefix".to_string(),
+			suffix: "suffix".to_string(),
+		};
+		reserialize(message);
+	}
 
 	#[test]
-    fn test_state_message_engine() {
-        let json = r#"{"type":"MSG","game":42,"turn":0,"players":["p1","p2"],"state":{"STATE":{}}}"#;
-		let message: StateMessageEngine = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
-
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.game,42);
-		assert_eq!(message.turn,0);
-		assert_eq!(message.players,["p1","p2"]);
-    }
-
+	fn test_state_message() {
+	let message = XMessage::State{
+			game: 42,
+			key: "key".to_string(),
+			turn: 0,
+			r#move: true,
+			state : json!({"This can be": "anything"})
+		};
+		reserialize(message);
+	}
 
 	#[test]
-    fn test_stop_message() {
-		let json = r#"{"type":"MSG","game":42}"#;
-        let message: StopMessage = serde_json::from_str(json).unwrap();
-		assert_eq!(reserialize(&message, json), true);
+	fn test_state_message_engine() {
+	let message = XMessage::EngineState{
+			game: 42,
+			turn: 0,
+			state : json!({"This can be": "anything"}),
+			players: vec!["p1".to_string(),"p2".to_string()],
 
-		assert_eq!(message.msgType,"MSG");
-		assert_eq!(message.game,42);
-    }
+		};
+		reserialize(message);
+	}
+
+	#[test]
+	fn test_stop_message() {
+	let message = XMessage::Stop{
+			game: 42,
+		};
+		reserialize(message);
+	}
 }
